a0562f10c550cbc3102eb5ba753dd1f14e36465c
arthurseredaa <neonbetcy@gmail.com>
2021-02-23 16:35:07 +0200

    Sip provider and login bt token

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,64 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route, useHistory } from "react-router-dom";
 import { Login } from "./components/Login/Login";
 import { userLoginWithToken } from "./redux/actions/login";
+import { SipProvider } from "react-sip";
+import { Phone } from "./components/Phone/Phone";
 
 function App() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const isLogin = useSelector((state) => state.user.isLogin);
   const isLoading = useSelector((state) => state.app.isLoading);
+  const config = useSelector((state) => state.phone.config);
+
   const token = localStorage.getItem("token");
-  // localStorage.clear()
+
   useEffect(() => {
-    if (token) {
+    if (!isLogin && token) {
       dispatch(userLoginWithToken());
+    } else if (!isLogin && !token) {
+      history.push("/login");
     }
-  }, [dispatch, token]);
+  }, [dispatch, token, history, isLogin]);
+
+  const Home = () => {
+    return (
+      <div className="App">
+        <h1>Home</h1>
+        {config && <Phone config={config} />}
+      </div>
+    );
+  };
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
 
   return (
     <>
-      <Route
-        exact
-        path="/login"
-        render={() => <Login isLogin={isLogin} isLoading={isLoading} />}
-      />
-      <Route exact path="/">
-        {!token && !isLogin ? <Redirect to="/login" /> : <Home />}
-      </Route>
+      <SipProvider
+        host={config && config.host}
+        uri={config && config.uri}
+        user={config && config.name}
+        pathname={config && config.via_transport}
+        password={config && config.password}
+        realm={config && config.realm}
+        debug={false}
+      >
+        <Route
+          exact
+          path="/login"
+          render={() => <Login isLogin={isLogin} isLoading={isLoading} />}
+        />
+        <Route exact path="/">
+          {!token && !isLogin ? <Redirect to="/login" /> : <Home />}
+        </Route>
+      </SipProvider>
     </>
   );
 }
-
-const Home = () => {
-  return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
-  );
-};
 
 export default App;
