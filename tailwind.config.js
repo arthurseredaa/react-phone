f6f76bd47e32b503ec1455e11ad1c1a5590f3066
arthurseredaa <neonbetcy@gmail.com>
2021-02-23 16:33:47 +0200

    Add some assets and additional changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    minHeight: {
+      "200": "400px"
+    },
+    colors: {
+      ...defaultColors,
+      figmaBlue: "#554AFF",
+      figmaGray: "#F0F1F8",
+      figmaGreen: "#6DD453",
+      figmaGreenHover: "#70DE3C"
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
