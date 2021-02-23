827bc3dfa5e1fd0567be21877e5146df3c725f53
arthurseredaa <neonbetcy@gmail.com>
2021-02-23 16:37:15 +0200

    Create phone reducer

---
+++ src/redux/reducers/phone.js
@@ -0,0 +1,20 @@
+import { CLEAR_PHONE_NUMBER, SET_CONFIG, SET_PHONE_NUMBER } from "../types";
+
+let initialState = {
+  config: null,
+  phoneNumber: "+380680205003",
+  callStatus: null,
+};
+
+export const phoneReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CONFIG:
+      return { ...state, config: action.config };
+    case SET_PHONE_NUMBER:
+      return { ...state, phoneNumber: `${state.phoneNumber + action.number}` };
+    case CLEAR_PHONE_NUMBER:
+      return {...state, phoneNumber: state.phoneNumber.slice(0, -1)}
+    default:
+      return state;
+  }
+};
