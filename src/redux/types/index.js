3dab919e63b367e1e467432ec85ba6d264a33d63
arthurseredaa <neonbetcy@gmail.com>
2021-02-23 16:37:39 +0200

    Create new types

--- src/redux/types/index.js
+++ src/redux/types/index.js
@@ -3,3 +3,7 @@ export const SET_IS_LOGIN = "SET_IS_LOGIN";
 export const SET_LOADING = "SET_LOADING";
 
 export const SET_CONFIG = "SET_CONFIG";
+
+export const SET_PHONE_NUMBER = "SET_PHONE_NUMBER";
+
+export const CLEAR_PHONE_NUMBER = "CLEAR_PHONE_NUMBER";
