fb2cd300ab390c38161ea229cab7543a897460f3
arthurseredaa <neonbetcy@gmail.com>
2021-02-23 16:37:27 +0200

    Refactoring

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,16 +1,14 @@
-import { SET_IS_LOGIN, SET_CONFIG } from "../types";
+import { SET_IS_LOGIN } from "../types";
 
 const initialState = {
   isLogin: false,
-  config: null,
+  // config: null,
 };
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_IS_LOGIN:
       return { ...state, isLogin: action.bool };
-    case SET_CONFIG:
-      return { ...state, config: action.config };
     default:
       return state;
   }
